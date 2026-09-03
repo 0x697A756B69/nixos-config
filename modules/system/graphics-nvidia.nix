4911908c19cf7c63ef8cfe41230b8aa924ea83f2
0x697A756B69 <mohamed.elmokhtari26@gmail.com>
2026-09-04 01:07:14 +0200

    disable 32bit graphics

--- modules/system/graphics-nvidia.nix
+++ modules/system/graphics-nvidia.nix
@@ -1,14 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # Configure Nvidia Driver
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true;
   hardware.nvidia.modesetting.enable = true;
-  hardware.graphics.enable32Bit = true;
 
-  # Configure Hyprland + Nvidia
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     GBM_BACKEND = "nvidia-drm";
