bddb2fc7be9e9b07ae939cd274aa9a42922fac21
0x697A756B69 <mohamed.elmokhtari26@gmail.com>
2026-09-04 01:05:33 +0200

    remove minecraft glfw patch

--- modules/system/misc-programs.nix
+++ modules/system/misc-programs.nix
@@ -1,13 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Flakes settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Install firefox.
   programs.firefox.enable = true;
 
   programs.nix-ld.enable = true;
@@ -15,19 +12,5 @@
     stdenv.cc.cc
     zlib
     glib
-    (glfw.override { withMinecraftPatch = true; })
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 }
