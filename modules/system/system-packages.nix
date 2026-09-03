5f569289f57c3ec93d942bb087b1a1251d106fe5
0x697A756B69 <mohamed.elmokhtari26@gmail.com>
2026-09-04 01:05:32 +0200

    remove minecraft packages

--- modules/system/system-packages.nix
+++ modules/system/system-packages.nix
@@ -1,36 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-
   vscodium
   nixd
   nixpkgs-fmt
   just
   opencode-desktop
-  protonup-qt
-  (symlinkJoin {
-    name = "modrinth-app";
-    paths = [ modrinth-app ];
-    nativeBuildInputs = [ makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/ModrinthApp \
-        --set WEBKIT_DISABLE_DMABUF_RENDERER 1
-    '';
-  })
-  temurin-jre-bin-21  # Minecraft
-  temurin-jre-bin-25  # Minecraft
   git
   curl
   unzip
   cabextract
   p7zip
   file
-  steamtinkerlaunch
   btop
   claude-code
   sbctl
